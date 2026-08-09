690fe291f7ba416a410e5d0d92502f4659f19a49
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2026-08-09 07:18:32 +0300

    chore: release v1.6.0

--- dch.c
+++ dch.c
@@ -19,7 +19,7 @@
 #include <sys/un.h>
 
 /* dch's own version, independent of the dtach base (PACKAGE_VERSION). */
-#define DCH_VERSION "1.5.0"
+#define DCH_VERSION "1.6.0"
 
 /* Shared globals (declared in dtach.h, used by attach.c/master.c). */
 const char copyright[] = "dch - based on dtach " PACKAGE_VERSION
